ed72a39b93d79bf695525f411f1232fd9aea3ed7
Administrator <Administrator@ugs>
2021-01-01 22:51:42 +0800

    报表菜单

---
+++ src/com/fun/tc/nc/handlers/CNCProgramCatalogueHandler.java
@@ -0,0 +1,15 @@
+package com.fun.tc.nc.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CNCProgramCatalogueHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		System.out.println("123");
+		return null;
+	}
+
+}
---
+++ src/com/fun/tc/nc/handlers/ToolingCatalogueHandler.java
@@ -0,0 +1,15 @@
+package com.fun.tc.nc.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ToolingCatalogueHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		System.out.println("456");
+		return null;
+	}
+
+}
