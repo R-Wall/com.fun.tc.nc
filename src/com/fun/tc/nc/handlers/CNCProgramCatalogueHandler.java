018f67ff3ae483c8f43e897a36331465ce22b73f
Administrator <Administrator@ugs>
2021-01-04 09:38:12 +0800

    报表修改

--- src/com/fun/tc/nc/handlers/CNCProgramCatalogueHandler.java
+++ src/com/fun/tc/nc/handlers/CNCProgramCatalogueHandler.java
@@ -1,14 +1,20 @@
 package com.fun.tc.nc.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
 import com.fun.tc.nc.until.MyWriteExcelUntil;
+import com.teamcenter.rac.aif.kernel.AIFComponentContext;
 import com.teamcenter.rac.aif.kernel.InterfaceAIFComponent;
 import com.teamcenter.rac.aifrcp.AIFUtility;
+import com.teamcenter.rac.kernel.TCComponent;
 import com.teamcenter.rac.kernel.TCComponentBOPLine;
 import com.teamcenter.rac.kernel.TCComponentItemRevision;
+import com.teamcenter.rac.kernel.TCException;
 import com.teamcenter.rac.util.MessageBox;
 
 public class CNCProgramCatalogueHandler extends AbstractHandler{
@@ -20,20 +26,64 @@ public class CNCProgramCatalogueHandler extends AbstractHandler{
 			TCComponentBOPLine comp  = (TCComponentBOPLine) aifcomp;
 			TCComponentItemRevision rev = comp.getItemRevision();
 			String type = rev.getType();
-			if ("MEProcessRevision".equals(type)||"MENCMachining Revision".equals(type)) {
-				String path = MyWriteExcelUntil.writeCNCProgramCatalogueExcel(rev);
-				MyWriteExcelUntil.addToNewStuff(path);
-				MessageBox.post("报表输出成功！","提示",MessageBox.INFORMATION);
-
-			}else {
-				MessageBox.post("请选择数控程序集或数控工序进行操作", "提示", MessageBox.INFORMATION);
+			if (!type.equals("MEProcessRevision") && !type.equals("AE8RootProcess Revision")) {
+				MessageBox.post("请选择总工艺或数控程序集进行操作", "提示", MessageBox.INFORMATION);
 				return null;
-			}		
+			}
+			String path = MyWriteExcelUntil.writeCNCProgramCatalogueExcel(getValues(comp));
+			MyWriteExcelUntil.addToNewStuff(path);
+			MessageBox.post("报表输出成功！","提示",MessageBox.INFORMATION);
 		} catch (Exception e) {
 			MessageBox.post(e.toString(),"错误",MessageBox.ERROR);
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public List<String[]> getValues(TCComponentBOPLine line) throws TCException{
+		TCComponentItemRevision rev = line.getItemRevision();
+		String type = rev.getType();
+		List<TCComponentItemRevision> revs = new ArrayList<>();
+		if (type.equals("AE8RootProcess Revision")) {
+			AIFComponentContext[] contexts = line.getChildren();
+			for (AIFComponentContext context : contexts) {
+				TCComponentBOPLine bopLine = (TCComponentBOPLine) context.getComponent();
+				rev = bopLine.getItemRevision();
+				if (bopLine.getItemRevision().getType().equals("MEProcessRevision")) {
+					revs.add(rev);
+				}
+			}
+		} else if (type.equals("MEProcessRevision")) {
+			
+			revs.add(rev);
+		}
+		
+		return getValues(revs);
+	}
+
+	public List<String[]> getValues(List<TCComponentItemRevision> revs) throws TCException{
+		List<String[]> values = new ArrayList<String[]>();
+		int num = 0;
+		for (int i = 0; i < revs.size(); i++) {
+			TCComponentItemRevision rev = revs.get(i);
+			TCComponent form = rev.getRelatedComponent("IMAN_master_form_rev");
+			if (form == null) {
+				continue;
+			}
+			String[] value = new String[10];
+			value[0] = ++num + "";
+			value[1] = form.getProperty("ae8_att1");
+			value[2] = form.getProperty("ae8_att4");
+			value[3] = "";
+			value[4] = "";
+			value[5] = "";
+			value[6] = "";
+			value[7] = "";
+			value[8] = "";
+			value[9] = "";
+			values.add(value);
+		}
+		return values;
 	}
 
 }
--- src/com/fun/tc/nc/handlers/ToolingCatalogueHandler.java
+++ src/com/fun/tc/nc/handlers/ToolingCatalogueHandler.java
@@ -1,17 +1,20 @@
 package com.fun.tc.nc.handlers;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-import com.fun.tc.nc.until.MyDatasetUtil;
 import com.fun.tc.nc.until.MyWriteExcelUntil;
+import com.teamcenter.rac.aif.kernel.AIFComponentContext;
 import com.teamcenter.rac.aif.kernel.InterfaceAIFComponent;
 import com.teamcenter.rac.aifrcp.AIFUtility;
+import com.teamcenter.rac.kernel.TCComponent;
 import com.teamcenter.rac.kernel.TCComponentBOPLine;
 import com.teamcenter.rac.kernel.TCComponentItemRevision;
+import com.teamcenter.rac.kernel.TCException;
 import com.teamcenter.rac.util.MessageBox;
 
 public class ToolingCatalogueHandler extends AbstractHandler{
@@ -23,20 +26,71 @@ public class ToolingCatalogueHandler extends AbstractHandler{
 			TCComponentBOPLine comp  = (TCComponentBOPLine) aifcomp;
 			TCComponentItemRevision rev = comp.getItemRevision();
 			String type = rev.getType();
-			if (type.contains("Process Revision")||type.contains("Machining Revision")) {
-				String path = MyWriteExcelUntil.writeToolingCatalogueExcel(rev);
-				MyWriteExcelUntil.addToNewStuff(path);
-				MessageBox.post("报表输出成功！","提示",MessageBox.INFORMATION);
-			}else {
-				MessageBox.post("请选择工艺或工序进行操作", "提示", MessageBox.INFORMATION);
+			if (!type.equals("MEProcessRevision") && !type.equals("MENCMachining Revision")) {
+				MessageBox.post("请选择数控程序集或数控工序进行操作", "提示", MessageBox.INFORMATION);
 				return null;
 			}
 			
+			String path = MyWriteExcelUntil.writeToolingCatalogueExcel(getValues(comp));
+			MyWriteExcelUntil.addToNewStuff(path);
+			MessageBox.post("报表输出成功！","提示",MessageBox.INFORMATION);
 		} catch (Exception e) {
-			MessageBox.post(e.toString(),"错误",MessageBox.ERROR);
+			MessageBox.post(e);
 			e.printStackTrace();
 		}
 		return null;
 	}
+	
+	public List<String[]> getValues(TCComponentBOPLine line) throws TCException{
+		TCComponentItemRevision rev = line.getItemRevision();
+		String type = rev.getType();
+		List<TCComponentBOPLine> bops = new ArrayList<>();
+		if (type.equals("MEProcessRevision")) {
+			AIFComponentContext[] contexts = line.getChildren();
+			for (AIFComponentContext context : contexts) {
+				TCComponentBOPLine bopLine = (TCComponentBOPLine) context.getComponent();
+				if (bopLine.getItemRevision().getType().equals("MENCMachining Revision")) {
+					bops.add(bopLine);
+				}
+			}
+		} else if (type.equals("MENCMachining Revision")) {
+			bops.add(line);
+		}
+		return getValues(bops);
+	}
 
+	public List<String[]> getValues(List<TCComponentBOPLine> bops) throws TCException{
+		List<String[]> values = new ArrayList<String[]>();
+		int num = 0;
+		for (int i = 0; i < bops.size(); i++) {
+			TCComponentBOPLine bop = bops.get(i);
+			TCComponentItemRevision topRev = bop.getItemRevision();
+			AIFComponentContext[] contexts = bop.getChildren();
+			for (AIFComponentContext context : contexts) {
+				TCComponentBOPLine bopLine = (TCComponentBOPLine) context.getComponent();
+				TCComponentItemRevision rev = bopLine.getItemRevision();
+				if (rev.getType().equals("AE8Tool Revision")) {
+					TCComponent form = rev.getRelatedComponent("IMAN_master_form_rev");
+					if (form == null) {
+						continue;
+					}
+					String[] value = new String[11];
+					value[0] = ++num + "";
+					value[1] = topRev.toDisplayString();
+					value[2] = form.getProperty("ae8gx_no");
+					value[3] = "";
+					value[4] = "";
+					value[5] = "";
+					value[6] = "";
+					value[7] = "";
+					value[8] = "";
+					value[9] = "";
+					value[10] = "";
+					values.add(value);
+				}
+			}
+		}
+		return values;
+	}
+	
 }
